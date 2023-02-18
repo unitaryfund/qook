e71d42420088fbfe4cfbc37fb1b8e1fb1cd53f45
Daniel Strano <stranoj@gmail.com>
2023-02-18 13:50:17 -0500

    QrackError

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod pauli;
+pub mod qrack_error;
 pub mod qrack_system;
---
+++ src/qrack_error.rs
@@ -0,0 +1,14 @@
+// (C) Daniel Strano and the Qrack contributors 2017-2023. All rights reserved.
+//
+// Pauli operators are specified for "b" (or "basis") parameters.
+//
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file or at https://opensource.org/licenses/MIT.
+
+pub struct QrackError;
+
+impl std::fmt::Display for QrackError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Qrack C++ library raised exception")
+    }
+}
