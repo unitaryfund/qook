4df4f43f0468490d31d9ea7832ab48beca3b1ac9
Daniel Strano <stranoj@gmail.com>
2023-02-18 07:25:23 -0500

    pauli.rs and pub mod bindings

---
+++ src/pauli.rs
@@ -0,0 +1,17 @@
+// (C) Daniel Strano and the Qrack contributors 2017-2023. All rights reserved.
+//
+// Pauli operators are specified for "b" (or "basis") parameters.
+//
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file or at https://opensource.org/licenses/MIT.
+
+enum Pauli {
+    // Pauli Identity operator. Corresponds to Q# constant "PauliI."
+    PauliI = 0
+    // Pauli X operator. Corresponds to Q# constant "PauliX."
+    PauliX = 1
+    // Pauli Y operator. Corresponds to Q# constant "PauliY."
+    PauliY = 3
+    // Pauli Z operator. Corresponds to Q# constant "PauliZ."
+    PauliZ = 2
+}
