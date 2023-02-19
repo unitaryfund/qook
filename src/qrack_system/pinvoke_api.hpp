364fc95146be2e8cf927055a3e6ccd51081d0c29
Daniel Strano <stranoj@gmail.com>
2023-02-19 16:32:37 -0500

    Retain license notice, but note additional author contribution

--- src/qrack_system/pinvoke_api.hpp
+++ src/qrack_system/pinvoke_api.hpp
@@ -1,5 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+//
+// (Extensively modified and adapted in unitaryfund/qrack, by Daniel Strano)
 
 #pragma once
 
