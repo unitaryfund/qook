8e3ad96b9625da44f47896546d9c060ab2a94d4f
Daniel Strano <stranoj@gmail.com>
2023-02-20 11:26:14 -0500

    OS-agnostic build.rs

--- src/build.rs
+++ src/build.rs
@@ -9,7 +9,7 @@ fn main() {
     println!("cargo:rustc-link-lib=qrack_pinvoke");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed=pinvoke_api.hpp");
+    println!("cargo:rerun-if-changed=qrack/pinvoke_api.hpp");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -17,7 +17,7 @@ fn main() {
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .header("pinvoke_api.hpp")
+        .header("qrack/pinvoke_api.hpp")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
