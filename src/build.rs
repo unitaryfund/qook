fe9f4abe5eb5b684f9daa737c1dc36974b602f3a
Daniel Strano <stranoj@gmail.com>
2023-02-20 12:17:56 -0500

    Fix Cargo.toml

--- src/build.rs
+++ src/build.rs
@@ -1,15 +1,12 @@
 extern crate bindgen;
 
-use std::env;
-use std::path::PathBuf;
-
 fn main() {
     // Tell cargo to tell rustc to link the system bzip2
     // shared library.
     println!("cargo:rustc-link-lib=qrack_pinvoke");
 
     // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed=qrack/pinvoke_api.hpp");
+    println!("cargo:rerun-if-changed=include/pinvoke_api.hpp", );
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -17,17 +14,17 @@ fn main() {
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate
         // bindings for.
-        .header("qrack/pinvoke_api.hpp")
+        .header("include/pinvoke_api.hpp")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        // Finish the builder and generate the bindings.
+        // Finish the builder and generate the bindings.A
         .generate()
         // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
     // Write the bindings to the qrack_system.rs file.
     bindings
-        .write_to_file(out_path.join("qrack_system.rs"))
+        .write_to_file("src/qrack_system.rs")
         .expect("Couldn't write bindings!");
 }
