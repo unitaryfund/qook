8bb4271c3abc47b8880c03ec80a7cc2988aaf14a
Daniel Strano <stranoj@gmail.com>
2023-02-21 10:56:07 -0500

    Fix warnings; improve example (v0.1.2)

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,9 +1,14 @@
 extern crate qook;
 use qook::qrack_simulator;
+use std::f64::consts::FRAC_1_SQRT_2;
 fn main() {
+    // *ALL* QrackSimulator methods can report exception in the underlying C++, hence "unwrap()."
     let q_reg = qrack_simulator::QrackSimulator::new(12).unwrap();
-    for i in 0..12 {
+    for i in 0..11 {
         q_reg.h(i).unwrap();
     }
+    // Test mtrx array parameter
+    q_reg.mtrx(&[FRAC_1_SQRT_2, 0.0, FRAC_1_SQRT_2, 0.0, FRAC_1_SQRT_2, 0.0, -FRAC_1_SQRT_2, 0.0], 11).unwrap();
+    println!("Probability of highest bit for |1> state: {}", q_reg.prob(11).unwrap());
     println!("Random number less than 2^12: {}", q_reg.m_all().unwrap());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod pauli;
 pub mod qrack_error;
+#[allow(non_camel_case_types)]
 pub mod qrack_system;
 pub mod qrack_simulator;
